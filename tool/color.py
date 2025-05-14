c69a7283caf1ef5d218ee91971cb56c8f44f8bba
wxl0430 <141288415+wxl0430@users.noreply.github.com>
2025-05-14 20:03:05 +0800

    refactor: 删除旧文件

--- tool/color.py
+++
@@ -1,6 +0,0 @@
-def hex_to_rgb(hex_color: str) -> tuple:
-    hex_color = hex_color.lstrip('#')
-    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
-
-def rgb_to_hex(rgb_color: tuple) -> str:
-    return '#{:02x}{:02x}{:02x}'.format(rgb_color[0], rgb_color[1], rgb_color[2])
