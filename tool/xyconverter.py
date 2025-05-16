3b98a753bc43526f872e54aca8bb97e33e47cf8c
wxl0430 <141288415+wxl0430@users.noreply.github.com>
2025-05-16 19:41:51 +0800

    feat: 添加屏幕位置显示

--- tool/xyconverter.py
+++ tool/xyconverter.py
@@ -3,20 +3,16 @@ from tool.database import *
 缩放 scaling*built_in_scaling/100 倍
 0,0 对应 screenx,screeny
 """
-def databasexytoscreenxy(x : int, y : int) -> tuple:
+def databasexytoscreenxy(x : float, y : float) -> tuple:
     nowscaling = getnowscaling()
     nowscreenpos = getscreenpos()
     nowx = x * nowscaling + nowscreenpos[0]
     nowy = y * nowscaling + nowscreenpos[1]
-    nowx = int(nowx)
-    nowy = int(nowy)
     return (nowx, nowy)
 
-def screenxytodatabasexy(x : int, y : int) -> tuple:
+def screenxytodatabasexy(x : float, y : float) -> tuple:
     nowscaling = getnowscaling()
     nowscreenpos = getscreenpos()
     nowx = (x - nowscreenpos[0]) / nowscaling  
     nowy = (y - nowscreenpos[1]) / nowscaling
-    nowx = int(nowx)
-    nowy = int(nowy)
     return (nowx, nowy)
